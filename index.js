a39b07da370c57d95d0c35e97763477d0eb3d6fe
rogertavaress <rogerioctf@gmail.com>
2019-09-04 12:51:03 -0300

    feat: Criado os metodos Read(get) e Write(post)

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Desafio 01
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Metodo: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd("Request");
+});
+
+const projects = [];
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    tasks: []
+  });
+
+  return res.json(projects);
+});
+
+server.get("/projects", () => {
+  return res.json(projects);
+});
+
+server.listen(3000);
